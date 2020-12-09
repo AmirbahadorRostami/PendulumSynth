e5bfeb5e5118d1658ccbddc3ca95c0433665c301
amirbahador <amirbahador@amirbahadors-MacBook-Pro-2.local>
2020-12-09 16:35:13 -0500

    Ela expirement: csv Recorder

--- MasterPC.py
+++ MasterPC.py
@@ -1,31 +1,27 @@
 from pythonosc import dispatcher
 from pythonosc import osc_server
+import csv
+import time as Time
 
 
 
-
-
-def print_volume_handler(unused_addr, args, volume):
-  print("[{0}] ~ {1}".format(args[0], volume))
-
-def print_compute_handler(unused_addr, args, volume):
-  try:
-    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
-  except ValueError: pass
-
+def InComingRow(args):
+    with open('eggs.csv', 'a', newline='') as csvfile:
+        writer_ = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer_.writerow([args])
+    print(args)
 
 
 MainPC_IP = "192.168.0.10"
 MainPC_Port = 6000
 
-GamaSpcae_IP = "192.168.0.37"
-GamaSpace_Port = 3000
-
-
 dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/Data", print)
-#dispatcher.map("/volume", print_volume_handler, "Volume")
-#dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+dispatcher.map("/Data", InComingRow)
+
+with open('eggs.csv', 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Time']+['CurrentNote(SSU_1)']+['CurrentNote(SSU_2)']+['isMathced'])
+
 
 server = osc_server.ThreadingOSCUDPServer((MainPC_IP, MainPC_Port), dispatcher)
 print("Serving on {}".format(server.server_address))
